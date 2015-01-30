976662aba49227e059aa15ef4fab4d140b49ca52
Paul Nechifor <paul@nechifor.net>
2015-01-30 08:17:22 +0000

    Add initial service.

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -17,9 +17,20 @@ setup_docker() {
   gpasswd -a vagrant docker # User docker without root for vagrant.
 }
 
+setup_service() {
+  cd /vagrant
+  npm install
+  touch /var/log/docker-sandbox
+  chown vagrant:vagrant /var/log/docker-sandbox
+  cp /vagrant/scripts/docker-sandbox /etc/init.d
+  chkconfig --add docker-sandbox
+  service docker-sandbox start
+}
+
 main() {
   install_packages
   setup_docker
+  setup_service
 }
 
 main "$@"
